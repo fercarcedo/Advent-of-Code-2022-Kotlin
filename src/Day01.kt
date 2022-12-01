ec686cfc465b5c527485f2cdb359f67f75371956
Fernando García Álvarez <fercarcedo@gmail.com>
2022-12-01 23:00:52 +0100

    Add solutions for first day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun List<String>.splitByBlanksToInts() = this.fold(mutableListOf(mutableListOf<Int>())) { acc, next ->
+        if (next.isNotBlank()) {
+            acc[acc.lastIndex].add(next.toInt())
+        } else {
+            acc.add(mutableListOf())
+        }
+        acc
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.splitByBlanksToInts()
+        .maxOfOrNull { it.sum() }
 
-    // test if implementation meets criteria from the description, like:
+    fun part2(input: List<String>) = input.splitByBlanksToInts()
+        .map { it.sum() }
+        .sortedDescending()
+        .take(3)
+        .sum()
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
